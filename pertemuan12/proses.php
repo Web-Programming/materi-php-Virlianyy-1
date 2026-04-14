50362b597eed9aaf6a693cd92b08da11b5d4cbee
Virlianyy <virlianyjessicaputri23@gmail.com>
2026-04-14 22:01:46 +0700

    update proses.php

--- pertemuan12/proses.php
+++ pertemuan12/proses.php
@@ -44,5 +44,18 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
                 <?php foreach ($postErrors as $error): ?>
                     <li><?=htmlspecialchars($error, ENT_QUOTES, 'UTF-8') ?><li>
                 <?php endforeach; ?>
+            </ul>
+        </div>
+    <?php endif; ?>
+    <?php if ($postSuccess): ?>
+        <div class="success">Data berhasil dikirim dengan method POST.</div>
+        <div class="result">
+            <strong>Hasil POST:</strong><br>
+            Nama: <?= htmlspecialchars($nama, ENT_QUOTES, 'UTF-8') ?><br>
+            Email: <?= htmlspecialchars($email, ENT_QUOTES, 'UTF-8') ?><br>
+            Pesan :<?= htmlspecialchars($pesan, ENT_QUOTES, 'UTF-8') ?><br>
+        </div>
+    <?php endif; ?>
+    <a href="index2.php">Kembali ke Form</a>
 </body>
 </html>
