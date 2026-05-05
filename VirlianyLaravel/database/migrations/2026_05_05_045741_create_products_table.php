9848939e7902a55b8ea09e4ae4d9d4c4f91de483
YoujoSenku <virlianyjessicaputri23@gmail.com>
2026-05-05 12:25:08 +0700

    Create 2026_05_05_045741_create_products_table.php

---
+++ VirlianyLaravel/database/migrations/2026_05_05_045741_create_products_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table){
+            $table->string('name',100);
+            $table->decimal('price',10, 2);
+            $table->text('description')->nullable();
+            $table->enum('status', ['new', 'used'])->default();
+        })
